740001e80db4d874719ee8a3bd7ceecdb795dde7
MJLaboratory <mjluckk@gmail.com>
2023-11-14 21:15:01 +0900

    일반로그인 대용 pincheck 추가

--- templates/auth.py
+++ templates/auth.py
@@ -49,5 +49,42 @@ class Register(Resource):
 
             return "오류"
 
+
+@auth_api.route('/pincheck', methods=['POST'])
+class Register(Resource):
+    def post(self): 
+        params = request.get_json()
+        id_ = params['id']
+        if id_ == '123456789':
+            return "success"
+        else: 
+            return "PIN 틀림"
     
-    
+        # nickname = params['nickname']
+        # email = params['email']
+        # gender = params['gender']
+        # ageRange = params['ageRange']
+        # print(id_)
+        # print(nickname)
+        # print(email)
+        # print(gender)
+        # print(ageRange)
+        # try: 
+        #     user = users.find_one({'id': id_})
+
+        #     if user is None: 
+        #         try: 
+        #             users.insert_one({'id': id_, 'nickname': nickname, 'email': email, 'gender': gender, 'ageRange': ageRange})
+
+        #             return "success"
+                
+        #         except Exception as e:
+        #             print(e)
+
+        #             return "오류"
+        #     else: 
+
+        #         return "success"
+        # except Exception as e:
+
+        #     return "오류"
